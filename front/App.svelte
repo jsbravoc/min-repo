b2bffe742f670b72eb1d01c12a87fc89c0d0770d
Juan Sebastian Bravo <jsbravoc@gmail.com>
2025-01-30 20:53:37 -0500

    Add minimal reproducible example

---
+++ front/App.svelte
@@ -0,0 +1,5 @@
+<script lang="ts">
+    import type {Obj as _works} from '@packages/types/src/obj';
+    import type {Obj} from '@packages/types/src';
+    
+</script>
---
+++ types/src/obj.ts
@@ -0,0 +1,3 @@
+export interface Obj {
+  prop1: string;
+}
